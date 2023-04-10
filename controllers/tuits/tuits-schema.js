19be975cd06559117dd031fa89d64cccfd63d525
unknown <davis.gi@northeastern.edu>
2023-04-09 21:56:10 -0400

    added tuit stuff:

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,13 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    topic: String,
+    userName: String,
+    time: String,
+    title: String,
+    image: String,
+    replies: Number,
+    retuits: Number,
+    handle: String,
 }, {collection: 'tuits'});
 export default schema;
